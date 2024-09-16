5a75dd8f6a2ec58fa5bad7706d8c350739fd663c
earocorn <amavb789@gmail.com>
2024-09-16 14:20:21 -0500

    so many problems

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
 import Systems from 'osh-js/source/core/sweapi/system/Systems';
 import SystemFilter from "osh-js/source/core/sweapi/system/SystemFilter";
 import DataStreamFilter from "osh-js/source/core/sweapi/datastream/DataStreamFilter";
@@ -8,10 +8,14 @@ import VideoView from 'osh-js/source/core/ui/view/video/VideoView';
 import VideoDataLayer from 'osh-js/source/core/ui/layer/VideoDataLayer';
 import SweApi from "osh-js/source/core/datasource/sweapi/SweApi.datasource"
 import {Mode} from 'osh-js/source/core/datasource/Mode';
+import PointMarkerLayer from "osh-js/source/core/ui/layer/PointMarkerLayer";
+import LeafletView from "osh-js/source/core/ui/view/map/LeafletView";
+import OpenLayerView from "osh-js/source/core/ui/view/map/OpenLayerView";
 import "./App.css";
-import MapComponent from './MapComponent';
 
 function App() {
+  const start = "1970-12-31T23:59:59Z";
+  const end = "2024-12-31T23:59:59Z";
 
   const networkOpts = {
     endpointUrl: `162.238.96.81:8781/sensorhub/api`,
@@ -28,64 +32,51 @@ function App() {
 
     async function fetchSystems() {
       // Get collection of top level systems by a tag (ex: "lane" to get systems with "Lane"/"lane" in the name) with ability to select pageSize in case of large amount of lanes
-      const availableSystemsCollection = await systems.searchSystems(new SystemFilter({ q:"lane" }), /*pageSize = */10);
+      const availableSystemsCollection = await systems.searchSystems(undefined, /*pageSize = */10);
       const availableSystems = await availableSystemsCollection.nextPage();
       console.log(availableSystems); // List of osh-js "System" objects
       
       const lane1 = availableSystems[1]; // Get whichever lane, filter by "properties.properties.uid" to ensure it matches "urn:osh:system:lane"
       console.log(lane1); 
 
-      const lane1SubsystemsCollection = await lane1.searchMembers(); // Returns collection of subsystems of the lane 
-      const lane1Subsystems = await lane1SubsystemsCollection.nextPage();
-      console.log(lane1Subsystems);
+      const locDatastreamCol = await lane1.searchDataStreams(new DataStreamFilter({ ObservationFilter: ["http://www.opengis.net/def/SensorLocation"] }));
+      const locDatastream = (await locDatastreamCol.nextPage())[0];
 
-      // Retrieve certain drivers from 
-      const rpmDriver = lane1Subsystems.find((system: typeof System) => system.properties.properties.uid.startsWith("urn:osh:sensor:rapiscan"));
-
-      // Retrieve ALL Datastreams from a system
-      const rpmDatastreamsCollection = await rpmDriver.searchDataStreams(undefined, /*pageSize = */30); // Get all datastreams 
-      // Filter by observed property, this yields only the "Occupancy" Datastream
-      const rpmOccupancyStreamsCollection = await rpmDriver.searchDataStreams(new DataStreamFilter({ observedProperty: ["http://www.opengis.net/def/occupancy"] }))
-      
-      const occupancyDatastream = (await rpmOccupancyStreamsCollection.nextPage())[0];
-
-      // You can directly subscribe to a datastreams observations
-      occupancyDatastream.streamObservations(undefined, (message: any) => {
-        console.log(message);
-      });
-
-      // Or create a SweApi for videostream observations
-      const videoDriver = lane1Subsystems.find((system: typeof System) => system.properties.properties.uid.startsWith("urn:osh:sensor:ffmpeg"));
-      const videoDatastreamsCol = await videoDriver.searchDataStreams(new DataStreamFilter({ ObservationFilter: ["http://www.opengis.net/def/Video"] }));
-      const videoDatastream = (await videoDatastreamsCol.nextPage())[0];
-
-      const source = new SweApi(videoDatastream.properties.name, {
-        protocol: videoDatastream.networkProperties.streamProtocol,
-        endpointUrl: videoDatastream.networkProperties.endpointUrl,
-        resource: `/datastreams/${videoDatastream.properties.id}/observations`,
-        mode: Mode.REAL_TIME,
+      const source = new SweApi(locDatastream.properties.name, {
+        protocol: locDatastream.networkProperties.streamProtocol,
+        endpointUrl: locDatastream.networkProperties.endpointUrl,
+        startTime: start,
+        endTime: end,
+        resource: `/datastreams/${locDatastream.properties.id}/observations`,
+        mode: Mode.BATCH,
         tls: false,
-        responseFormat: 'application/swe+binary',
-        connectorOpts: videoDatastream.networkProperties.connectorOpts
+        connectorOpts: locDatastream.networkProperties.connectorOpts
       });
+      console.log(source);
 
-      const view = new VideoView({
-                container: "video-container",
-                showTime: false,
-                showStats: false,
-                layers: [new VideoDataLayer({
-                    dataSourceId: [source.getId()],
-                    getFrameData: (rec: any) => rec.img,
-                    getTimestamp: (rec: any) => rec.time,
+      const view = new OpenLayerView({
+                container: "map-container",
+                autoZoomOnFirstMarker: true,
+                layers: [new PointMarkerLayer({
+                    dataSourceId: source.getId(),
+                    getLocation: (rec: any) =>({x: rec.location.lon, y: rec.location.lat}),
+                    icon: 'images/house.png',
+                    iconAnchor: [16, 64],
+                    iconSize: [32, 64],
+                    zIndex: 1,
+                    defaultToTerrainElevation: true,
+                    labelOffset: [-5,-15],
+                    onLeftClick: (markerId: any, markerObject: any, event: { latlng: any; containerPoint: any; }) => console.log(markerId, event.latlng, event.containerPoint),
+                    onRightClick: (markerId: string, billboard: any, event: { containerPoint: { x: number; y: number; }; }) => {
+                        const rect = document.getElementById('leafletMap').getBoundingClientRect();
+                        console.log("HELLO RIGHT")
+                    },
+                    onHover: (markerId: any, markerObject: any, event: { latlng: any; containerPoint: any; }) => console.log(markerId, event.latlng, event.containerPoint),
+                    
                 })]
             });
 
-
-      // Get all datastreams from a node paginated for whatever size
-      const datastreams = new DataStreams(networkOpts);
-      const allDatastreamsCol = await datastreams.searchDataStreams(undefined, /*pageSize = */100);
-      const allDatastreams = await allDatastreamsCol.nextPage();
-      console.log(allDatastreams); // Prints 86 individual datastreams that are directly subscribable
+      source.connect();
     }
 
     fetchSystems();
@@ -96,10 +87,9 @@ function App() {
   return (
     <div className="App">
       <h1>Lane: </h1>
-      {/* <div style={{ padding: 50 }}>
-          <div id="video-container" style={{ width: "100%", height: "100%" }}/>
-      </div> */}
-      <MapComponent/>
+      <div style={{ padding: 50 }}>
+          <div id="map-container" style={{ width: "100%", height: "100%" }}/>
+      </div>
     </div>
   );
 }
--- src/MapComponent.tsx
+++ src/MapComponent.tsx
@@ -1,68 +1,177 @@
-import React, {useEffect, useMemo, useRef, useState} from "react";
-import OpenLayerView from "osh-js/source/core/ui/view/map/OpenLayerView";
-import PointMarkerLayer from "osh-js/source/core/ui/layer/PointMarkerLayer";
-import SweApi from "osh-js/source/core/datasource/sweapi/SweApi.datasource";
-import {Mode} from "osh-js/source/core/datasource/Mode";
-import DataSynchronizer from "osh-js/source/core/timesync/DataSynchronizer";
-import LeafletView from "osh-js/source/core/ui/view/map/LeafletView";
+// import React, {useEffect, useMemo, useRef, useState} from "react";
+// import PointMarkerLayer from "osh-js/source/core/ui/layer/PointMarkerLayer";
+// import SweApi from "osh-js/source/core/datasource/sweapi/SweApi.datasource";
+// import {Mode} from "osh-js/source/core/datasource/Mode";
+// import DataSynchronizer from "osh-js/source/core/timesync/DataSynchronizer";
+// import LeafletView from "osh-js/source/core/ui/view/map/LeafletView";
+// import {useSelector} from "react-redux";
+// import Box from "@mui/material/Box";
+// import './Map.css';
+// export default function MapComponent(){
+//     // @ts-ignore
+//     const mapViewRef = useRef<LeafletView | null>(null);
+//     // @ts-ignore
+//     let pointMarkers: PointMarkerLayer[] = [];
+//     let locationDs: typeof SweApi[] = [];
+//     let gammaDs: typeof SweApi[] = [];
 
 
-export default function MapComponent(){
+//     // const [locationDs, setLocationDs] = useState<SweApi[]>([]);
+//     // const [pointMarkers, setPointMarkers] = useState<PointMarkerLayer[]>([]);
 
-    const locationDsId = "9tuonlfm6ulr6";
-    const start = useMemo(() => new Date((Date.now() - 600000)).toISOString(), []);
-    const end = "2024-12-31T23:59:59Z";
-    let server = `162.238.96.81:8781`;
-    const openLayerContainer = useRef(null);
-    
-    const [layer, setLayer] = useState(null);
+//     /*****************lane status******************/
+//     useEffect(() => {
+//         if(laneMap.size > 0){
+//             laneMap.forEach((value,key) => {
+//                 if(laneMap.has(key)){
+//                     let ds: LaneMapEntry = laneMap.get(key);
+//                     const rpmGamma = ds.datasourcesRealtime.filter((item) => item.name.includes("Gamma Count"));
+//                     gammaDs.push(rpmGamma);
 
-    useEffect(() => {
-        const pointMarkerLayer = new PointMarkerLayer({
-            labelOffset: [0, -30],
-            location: {
-                x: 34.735,
-                z: -86.7232
-            },
-            icon: 'images/house.png',
-            iconSize: [32, 64],
-            iconAnchor: [16, 65],
-            defaultToTerrainElevation: true,
-            name: "Oscar Location",
-            // label: "Oscar",
-            // iconScale: .05,
-            // color: '#93ce27'
-        });
-        setLayer(pointMarkerLayer);
-    }, []);
+//                 }
+//             });
+//         }
+//         console.log('gamma', gammaDs);
+//     },[laneMap]);
+
+//     /******************map view********************/
+//     useEffect(() =>{
+//         if(laneMap.size > 0 && !mapViewRef.current){
+//             console.log(laneMap)
+//             laneMap.forEach((value, key) =>{
+//                 if(laneMap.has(key)){
+//                     let ds: LaneMapEntry = laneMap.get(key);
+//                     const rpmLocation = ds.datasourcesBatch.filter((item) => item.name.includes('Sensor Location') && item.name.includes('Lane'));
+//                     locationDs.push(rpmLocation);
+//                     // locationDs.push(ds.datasourcesBatch[0]);
+//                 }
+//             })
+
+//             console.log(locationDs);
+//             if(locationDs.length > 0){
+//                 locationDs.map((loc) => {
+//                     const newPointMarker = new PointMarkerLayer({
+//                         // getMarkerId: (rec: any) => rec.id,
+//                         getLocation: (rec: any) =>({x: rec.location.lon, y: rec.location.lat, z: rec.location.alt}),
+//                         icon: '/point.png',
+//                         iconAnchor: [16, 16],
+//                         iconSize: [16, 16],
+//                         iconScale: 1.0,
+//                         labelSize: 16,
+//                         labelScale: 1.0,
+//                         zoomLevel: 15,
+//                         defaultToTerrainElevation: false,
+//                         labelOffset: [-5,-15],
+//                         dataSourceId: loc[0].getId(),
+//                         onLeftClick: (markerId: any, markerObject: any, event: { latlng: any; containerPoint: any; }) => console.log(markerId, event.latlng, event.containerPoint),
+//                         onRightClick: (markerId: string, billboard: any, event: { containerPoint: { x: number; y: number; }; }) => {
+//                             const rect = document.getElementById('leafletMap').getBoundingClientRect();
+//                             console.log("HELLO RIGHT")
+//                         },
+//                         onHover: (markerId: any, markerObject: any, event: { latlng: any; containerPoint: any; }) => console.log(markerId, event.latlng, event.containerPoint),
+                        
+//                     });
+//                     newPointMarker.init();
+//                     console.log(newPointMarker)
+//                     pointMarkers.push(newPointMarker);
+//                 });
+//                 console.log('pointmarkers', pointMarkers);
+//                 }
+//         }
+
+//         return () => {
+//             if (mapViewRef.current) {
+//                 mapViewRef.current.destroy();
+//                 mapViewRef.current = null;
+//             }
+//         }
+//     },[laneMap]);
+
+//     useEffect(() => {
+//         if(pointMarkers.length > 0) {
+
+//             pointMarkers.forEach((pm) => {
+//                 console.info(pm);
+//             });
+
+//             /*********************VIEW****************************/
+//             if (!mapViewRef.current) {
+//                 mapViewRef.current = new LeafletView({
+//                     container: "mapcontainer",
+//                     autoZoomOnFirstMarker: true,
+//                     layers: pointMarkers,
+//                 });
+//             }
+//             locationDs.map((ds) => ds[0].connect());
+//         }
+//     }, [pointMarkers]);
+
+//     /***************content in popup************/
+//     //TODO: replace video with lane video
+//     //TODO: replace status with lane status
+//     function getContent(laneName: string, status: string) {
+//         // let videoStream;
+//         // console.log('lanes with viedo', lanesWithVideo)
+//         // if(lanesWithVideo !== null){
+//         //     videoStream = lanesWithVideo[1].videoDatastreams;
+//         // }
+//         let videocomponent =  `<VideoComponent videoDatastreams={videoDatastreams[0]}/>`
+//         // let videoview = "<video> <source src=" + videocomponent + "</video>"
+
+//         // let videoview = "<source src=\"https://www.w3schools.com/html/mov_bbb.mp4\" type=\"video/mp4\" style='overflow: hidden'>"
+
+//         // create main div
+//         const div = document.createElement("div");
+//         div.className = 'point-popup';
+
+//         const laneNameEle = document.createElement("h3");
+//         laneNameEle.className = 'popup-text-lane';
+//         laneNameEle.textContent = laneName;
+
+//         const statusEle = document.createElement("h3");
+//         statusEle.className = 'popup-text-status';
+//         statusEle.textContent = `Status: ${status}`;
+
+//         const video = document.createElement("video");
+//         const source = document.createElement("source");
+
+//         source.src = 'https://www.w3schools.com/html/mov_bbb.mp4';
+//         // source.src = videocomponent;
+
+//         source.type = 'video/mp4';
+//         video.appendChild(source);
+
+//         //create button
+//         const button = document.createElement("button");
+//         button.className = 'popup-button';
+//         button.textContent = "LANE VIEW";
 
 
-    useEffect(() => {
-        if(layer !== null) {
-            const leafletView = new LeafletView({
-                container: "map-container",
-                layers: [layer],
-                autoZoomOnFirstMarker: true
-            });
-            console.log(layer)
-        }
-    }, [layer]);
+//         div.appendChild(laneNameEle);
+//         div.appendChild(statusEle);
+//         div.appendChild(video);
+//         div.appendChild(button);
 
-    // const handleSelectedMarker = (selection: any[]) =>{
-    //    const selectedMarkerId = selection[0];
-    //
-    // };
+//         return div.outerHTML;
+//         // "<div class='point-popup'>" +
+//         //     "<h3 class='popup-text-lane'>" + laneName + "</h3>" +
+//         //     "<h3 class='popup-text-status'>Status: "+ status +"</h3>" +
+//         //     "<video><source " + <VideoComponent videoDatastreams={videoDatastreams[0]}/> + "></video>" +
+//         //     // "<div class='video-container'>" + videocomponent + "</div>" +
+//         //     // "<video <source src=" +
+//         //
+//         // // + " type='video/h264' /></video>" +
+//         //
+//         // // <VideoComponent videoDatastreams={videoDatastreams[0]}/>
+//         //     // "<video autoplay> <source src='https://www.w3schools.com/html/mov_bbb.mp4' type='video/mp4'/></video>" +
+//         //     "<button class='popup-button' onclick='function()=>{window.location.href=`/lane-view`}'>LANE VIEW</button>" +
+//         // "</div>"
+//         // );
+//     }
 
-    // var popup = L.popup();
-    // function onMarkerClick(event: Event){
-    //     popup
-    //         .setContent("You clicked the map!")
-    //         .openOn(map);
-    // }
-
-    return (
-        <div>
-            <div id="map-container" ref={openLayerContainer}></div>
-        </div>
-    );
-}
+//     return (
+//         <div>
+//             <div id="mapcontainer"></div>
+//         </div>
+//     );
+// }
