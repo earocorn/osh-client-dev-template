3a3db267931a91df426a7f8fd44bd045e6cb6572
earocorn <amavb789@gmail.com>
2025-07-23 12:34:04 -0500

    Cesium stuff

--- src/App.tsx
+++ src/App.tsx
@@ -12,13 +12,20 @@ import Commands from "osh-js/source/core/consysapi/command/Commands.js"
 import ConSysApi from "osh-js/source/core/datasource/consysapi/ConSysApi.datasource.js"
 import { Mode } from "osh-js/source/core/datasource/Mode";
 import DataSynchronizer from "osh-js/source/core/timesync/DataSynchronizer.js"
-
+// Cesium
+import { Cartesian3, Ion, SceneMode, Terrain, } from "@cesium/engine";
+import "@cesium/engine/Source/Widget/CesiumWidget.css";
+import CesiumView from "osh-js/source/core/ui/view/map/CesiumView.js";
+import PointMarkerLayer from "osh-js/source/core/ui/layer/PointMarkerLayer";
 import "./App.css";
 
 function App() {
-
-  const networkOpts ={
-    endpointUrl: '104.179.198.202:8282/sensorhub/api',
+  // @ts-ignore
+  window.CESIUM_BASE_URL = './';
+  Ion.defaultAccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJkNGU1OTIxNi0yYWMyLTQ0MGItOWNhNi0xNjlhNWE4Njk2ZjgiLCJpZCI6MzIwOTY1LCJpYXQiOjE3NTIzOTY1MDh9.12Fr28_Y_zO-V0VxoUplDyEMJmFxrmPZnDj8Q3wmGZg";
+  const cesiumContainer = useRef(null);
+  const networkOpts = {
+    endpointUrl: 'localhost:8282/sensorhub/api',
     connectorOpts: {
         username: 'admin',
         password: 'oscar',
@@ -34,21 +41,22 @@ function App() {
 
   const [time, setTime] = useState("2025-04-23T16:51:53Z");
 
-  let videoSource: typeof ConSysApi = null;
-  let videoView: typeof VideoView = null;
-  let videoLayer: typeof VideoDataLayer = null;
+  let source: typeof ConSysApi = null;
+  let cesiumView: typeof CesiumView = null;
+  let pointMarkerLayer: typeof PointMarkerLayer = null;
   let ds: typeof DataStream = null;
-  let masterTimeController: typeof DataSynchronizer = null;
-
-  const [weatherObs, setWeatherObs] = useState("");
 
   useEffect(() => {
     async function fetchData() {
       //8m1cof6p2fcso
       //q5nuf1f2tmj0s
-      ds = await datastreams.getDataStreamById("fj6kdcao2hikc");
+      const dataStreamCollection = await datastreams.searchDataStreams();
+      const dataStreams = await dataStreamCollection.nextPage();
+      ds = dataStreams[0];
 
-      videoSource = new ConSysApi('video', {
+      console.log(ds.properties.name)
+
+      source = new ConSysApi('video', {
         protocol: ds.networkProperties.streamProtocol,
         endpointUrl: ds.networkProperties.endpointUrl,
         resource: `/datastreams/${ds.properties.id}/observations`,
@@ -61,90 +69,91 @@ function App() {
 
       console.log(ds);
 
-      videoLayer = new VideoDataLayer({
-        dataSourceId: videoSource.id,
-        getFrameData: (rec: any) => rec.img,
-        getTimestamp: (rec: any) => rec.timestamp,
-      })
-
-      videoView = new VideoView({
-        container: "video-view",
-        showStats: false,
-        showTime: false,
-        layers: [videoLayer]
+      pointMarkerLayer = new PointMarkerLayer({
+        label: "Unmanned System",
+        dataSourceId: source.id,
+        getLocation: {
+          dataSourceIds: [source.getId()],
+          handler: function (rec: any) {
+            return {
+              x: rec.Location.lon,
+              y: rec.Location.lat,
+              z: rec.Location.alt
+            };
+          }
+        },
+        getOrientation: {
+          dataSourceIds: [source.getId()],
+          handler: function (rec: any) {
+            return {
+              heading: rec.Orientation.yaw,
+              pitch: rec.Orientation.pitch,
+              roll: rec.Orientation.roll
+            };
+          }
+        },
+        icon: "images/drone.glb",
+        iconSize: [32, 64],
+        iconScale: 12,
+        color: '#FF8000',
+        name: 'Unmanned System'
       });
 
-      masterTimeController =  new DataSynchronizer({
-        replaySpeed: 1,
-        intervalRate: 5,
-        dataSources: [videoSource]
+      cesiumView = new CesiumView({
+        container: cesiumContainer.current.id,
+        layers: [pointMarkerLayer],
+        options: {
+          viewerProps: {
+            terrain: Terrain.fromWorldTerrain(),
+            sceneMode: SceneMode.SCENE3D,
+            // infoBox: false,
+            // geocoder: false,
+            timeline: false,
+            animation: false,
+            homeButton: false,
+            scene3DOnly: true,
+            // baseLayerPicker: false,
+            // sceneModePicker: false,
+            fullscreenButton: false,
+            // projectionPicker: false,
+            // selectionIndicator: false,
+            navigationHelpButton: true,
+            navigationInstructionsInitiallyVisible: true
+          }
+        }
+      });
+
+      const baseLayerPicker = cesiumView.viewer.baseLayerPicker;
+
+      const imageryProviders = baseLayerPicker.viewModel.imageryProviderViewModels;
+      baseLayerPicker.viewModel.selectedImagery =
+          imageryProviders.find((imageProviders: any) => imageProviders.name === "Bing Maps Aerial");
+
+      const terrainProviders = baseLayerPicker.viewModel.terrainProviderViewModels;
+      baseLayerPicker.viewModel.selectedTerrain =
+          terrainProviders.find((terrainProviders: any) => terrainProviders.name === "Cesium World Terrain");
+
+      const obsCollection = await observations.searchObservations();
+      const obs = await obsCollection.nextPage();
+      const firstObs = obs[0];
+      const location = firstObs.properties.result.Location;
+      console.log("obs: ", location);
+
+      // Center the camera on the UAV
+      cesiumView.viewer.camera.flyTo({
+        destination: Cartesian3.fromDegrees(location.lon, location.lat, location.alt + 500)
       });
     }
+
     fetchData();
   }, []);
 
-  function start() {
-    masterTimeController.connect();
-  }
-
-  function moveTime(newTime: String) {
-    console.log(masterTimeController);
-    masterTimeController.setTimeRange(
-        newTime,
-        "2025-04-23T17:52:32Z",
-        masterTimeController.getReplaySpeed(),
-        false
-    );
-  }
-
-  function printCurrentData() {
-    console.log(videoLayer.getProps())
-    console.log(videoLayer.getCurrentProps())
-  }
-  async function setFrame() {
-    //
-    // const sys = await systems.getSystemById("haso9uhmu6imu");
-    //
-    // const ds = await datastreams.getDataStreamById("mkin73ik1q45m");
-    //
-    // const obs = await ds.searchObservations(new ObservationFilter({
-    //   format: 'application/swe+binary',
-    //   phenomenonTime: `${/*startTime+500ms*/}/${/*endTime-500ms*/}`
-    // }), 1);
-    //
-    // const obsPage = await obs.nextPage();
-    //
-    // let imgBlob = new Blob([obsPage[3].img.data]);
-    // let url = window.URL.createObjectURL(imgBlob);
-    //
-    // var imgTag = document.getElementById("test");
-    // let oldBlobURL = imgTag.src;
-    // imgTag.src = url;
-  }
-
-  let frameSrc = "";
-
   function pause() {
-    // console.log(masterTimeController)
-    // masterTimeController.disconnect();
-    // console.log(masterTimeController)
-    // var img = document.getElementsByClassName("video-mjpeg");
-    // frameSrc = (img[0].src);
-    videoSource.disconnect();
+    source.disconnect();
   }
 
   function play() {
-    // console.log(masterTimeController);
-    // var img = document.getElementsByClassName("video-mjpeg");
-    //
-    // masterTimeController.connect().finally(() => {
-    //   if(img.length > 0) {
-    //     console.log("Setting src to ", img[0].src);
-    //     img[0].src = frameSrc;
-    //     console.log(frameSrc);
-    //   }
-    // });
-    videoSource.connect();
+    source.connect();
   }
 
   return (
@@ -154,11 +163,8 @@ function App() {
       {/*<div id='rainChart'>rain</div>*/}
       <button onClick={() => play()}>START</button>
       <button onClick={() => pause()}>PAUSE</button>
-      <button onClick={() => setFrame()}>SET FRAME</button>
-      <button onClick={() => moveTime(time)}>MOVE TIME</button>
-      <button onClick={() => printCurrentData()}>CURRENT DATA</button>
       <input type="text" value={time} onChange={e => setTime(e.target.value)}></input>
-      <div id='video-view' style={{ height:"100%", width:"100%"}}></div>
+      <div id='cesium-view' ref={cesiumContainer} style={{ height:"100%", width:"100%"}}></div>
       <img id='test'></img>
     </div>
     
